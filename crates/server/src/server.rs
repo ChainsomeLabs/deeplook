2c11841acfb118caca05d343a843a2ab899310bb
Chepelau <chepelau.a@gmail.com>
2025-07-16 17:21:19 +0000

    Add spread websockets

--- crates/server/src/server.rs
+++ crates/server/src/server.rs
@@ -81,6 +81,7 @@ pub const DEEP_SUPPLY_PATH: &str = "/deep_supply";
 pub const ORDER_FILLS_PATH: &str = "/order_fills/:pool_name";
 pub const WEBSOCKET_ORDERBOOK: &str = "/ws_orderbook/:pool_name";
 pub const WEBSOCKET_ORDERBOOK_BESTS: &str = "/ws_orderbook_bests/:pool_name";
+pub const WEBSOCKET_ORDERBOOK_SPREAD: &str = "/ws_orderbook_spread/:pool_name";
 
 // Data Aggregation
 pub const OHLCV_PATH: &str = "/ohlcv/:pool_name";
@@ -182,6 +183,7 @@ pub(crate) fn make_router(state: Arc<AppState>, rpc_url: Url) -> Router {
         .route(OBI, get(orderbook_imbalance))
         .route(WEBSOCKET_ORDERBOOK, get(orderbook_ws))
         .route(WEBSOCKET_ORDERBOOK_BESTS, get(orderbook_bests_ws))
+        .route(WEBSOCKET_ORDERBOOK_SPREAD, get(orderbook_spread_ws))
         .with_state((state.clone(), rpc_url));
 
     let aggregation_routes = Router::new()
@@ -1490,6 +1492,14 @@ async fn orderbook_bests_ws(
     ws.on_upgrade(move |socket| handle_bests_socket(socket, pool_name, state.0.clone()))
 }
 
+async fn orderbook_spread_ws(
+    ws: WebSocketUpgrade,
+    Path(pool_name): Path<String>,
+    State(state): State<(Arc<AppState>, Url)>,
+) -> impl IntoResponse {
+    ws.on_upgrade(move |socket| handle_spread_socket(socket, pool_name, state.0.clone()))
+}
+
 async fn handle_orderbook_socket(mut socket: WebSocket, pool_name: String, state: Arc<AppState>) {
     // Redis key that stores the order‑book JSON
     let redis_key = format!("orderbook::{}", pool_name);
@@ -1614,6 +1624,84 @@ async fn handle_bests_socket(mut socket: WebSocket, pool_name: String, state: Ar
     }
 }
 
+
+async fn handle_spread_socket(mut socket: WebSocket, pool_name: String, state: Arc<AppState>) {
+    // Redis key that stores the order‑book JSON
+    let redis_key = format!("orderbook::{}", pool_name);
+
+    // // Clone the async cache and extract the underlying Redis client
+    let cache = state.reader.cache.clone();
+    let mut pubsub = cache
+        .client
+        .get_async_pubsub()
+        .await
+        .expect("Failed getting pubsub");
+    let channel = format!("__keyspace@0__:{}", redis_key);
+
+    pubsub
+        .subscribe(&channel)
+        .await
+        .expect("Failed to subscribe to key‑space");
+
+    // Helper to grab latest JSON
+    let fetch_latest = || async {
+        cache
+            .get::<serde_json::Value>(&redis_key)
+            .await
+            .ok()
+            .flatten()
+    };
+
+    let mut last_sent = fetch_latest().await;
+
+    let bests = get_bests_from_redis_orderbook(last_sent.clone());
+    let spread = get_spread_from_bests(bests);
+
+    let stringified = serde_json::to_string(&spread);
+
+    if let Ok(message) = stringified {
+        let _ = socket.send(Message::Text(message)).await;
+    };
+    
+    // Stream of Redis events
+    let mut redis_stream = pubsub.on_message();
+
+    loop {
+        tokio::select! {
+            // Client closed WebSocket
+            maybe_msg = socket.recv().fuse() => {
+                if maybe_msg.is_none() {
+                    break;
+                }
+            }
+            // Redis published an event
+            Some(_msg) = redis_stream.next() => {
+                if let Some(current) = fetch_latest().await {
+                    if Some(&current) != last_sent.as_ref() {
+                        last_sent = Some(current.clone());
+
+                        let bests = get_bests_from_redis_orderbook(last_sent.clone());
+                        let spread = get_spread_from_bests(bests);
+                        let stringified = serde_json::to_string(&spread);
+                        
+                        if let Ok(message) = stringified {
+                            let _ = socket.send(Message::Text(message)).await;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn get_spread_from_bests(bests: Option<HashMap<String, HashMap<String, f64>>>) -> Option<f64> {
+    let map = bests?;
+
+    let ask_price = map.get("asks")?.get("price")?;
+    let bid_price = map.get("bids")?.get("price")?;
+        
+    Some(ask_price - bid_price)
+}
 
 fn get_bests_from_redis_orderbook(value: Option<Value>) -> Option<HashMap<String, HashMap<String, f64>>> {
     let ob = parse_orderbook_from_redis(value?)?;
